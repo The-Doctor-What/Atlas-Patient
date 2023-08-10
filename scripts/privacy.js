e30a4a4e8083384896f4d2fbad929cabda09cf0c
Hitok-Vandal <hitv@gmail.com>
2023-08-10 20:00:41 +0300

    Добавлена кнопка отображение пароля

--- scripts/privacy.js
+++ scripts/privacy.js
@@ -1,5 +1,7 @@
 const executeButton = document.getElementById("module-window-execute-button")
 const closeButton = document.getElementById("module-window-close-button")
+const passwordHideButton = document.getElementById("password-hide")
+const passwordRepeatHideButton = document.getElementById("password-repeat-hide")
 const personDateButton = document.getElementById("person-date")
 const regex = /^(\+7|7|8)?[\s\-]?\(?[489][0-9]{2}\)?[\s\-]?[0-9]{3}[\s\-]?[0-9]{2}[\s\-]?[0-9]{2}$/
 const windowContent = document.getElementById("module-window-content")
@@ -43,7 +45,20 @@ function openWindow() {
     moduleWindow.classList.remove("hidden")
 }
 
+function hidePassword() {
+    const password = document.getElementById("password")
+    if (password.type === "password") password.type = "text"
+    else password.type = "password"
+}
+
+function hidePasswordRepeat() {
+    const passwordRepeat = document.getElementById("password-repeat")
+    if (passwordRepeat.type === "password") passwordRepeat.type = "text"
+    else passwordRepeat.type = "password"
+}
+
 executeButton.addEventListener("click", save)
 closeButton.addEventListener("click", close)
-
 personDateButton.addEventListener("click", openWindow)
+passwordHideButton.addEventListener("click", hidePassword)
+passwordRepeatHideButton.addEventListener("click", hidePasswordRepeat)
